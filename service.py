3c435f8189b5ed7cdcff46689954bf6ed67f1e23
polyfloyd <floyd@polyfloyd.net>
2017-08-05 16:33:57 +0200

    Add a homescreen widget

---
+++ service.py
@@ -0,0 +1,14 @@
+import ugfx
+lenny = __import__(__file__.rsplit('/', 1)[0] + '/lenny')
+
+def setup():
+    pass
+
+def loop():
+    return 10*60*1000
+
+def draw(at_y):
+    (x, y, w, h) = lenny.render_creation(
+            lambda w, h: (lenny.BADGE_EINK_WIDTH // 2 - w // 2, at_y - h),
+            lenny.creation)
+    return [10*60*1000, h]
