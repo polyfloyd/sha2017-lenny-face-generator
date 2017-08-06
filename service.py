ca88559925d91c9a5500757a1278b8e8077f017d
polyfloyd <floyd@polyfloyd.net>
2017-08-06 15:03:55 +0200

    Remove service loop

--- service.py
+++ service.py
@@ -4,9 +4,6 @@ lenny = __import__(__file__.rsplit('/', 1)[0] + '/lenny')
 def setup():
     pass
 
-def loop():
-    return 10*60*1000
-
 def draw(at_y):
     (x, y, w, h) = lenny.render_creation(
             lambda w, h: (lenny.BADGE_EINK_WIDTH // 2 - w // 2, at_y - h),
